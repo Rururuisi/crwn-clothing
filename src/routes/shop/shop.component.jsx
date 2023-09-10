99fafda6813f141c46ede55f1c1b48dd404c4a12
Rururuisi <62675076+Rururuisi@users.noreply.github.com>
2023-09-10 18:35:34 -0400

    migrating categories selector

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -5,15 +5,15 @@ import { useDispatch } from "react-redux";
 import CategoriesPreview from "../../components/categories-preview/categories-preview.component";
 import Category from "../category/category.component";
 import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils.js";
-import { setCategoriesMap } from "../../store/categories/category.action";
+import { setCategories } from "../../store/categories/category.action";
 
 function Shop() {
     const dispatch = useDispatch();
 
     useEffect(() => {
         const getCategoryMap = async () => {
-            const categoryMap = await getCategoriesAndDocuments();
-            dispatch(setCategoriesMap(categoryMap));
+            const categoriesArray = await getCategoriesAndDocuments();
+            dispatch(setCategories(categoriesArray));
         };
         getCategoryMap();
     }, [dispatch]);
