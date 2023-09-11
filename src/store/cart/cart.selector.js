a11b5a20d35a4f702693dba01de334458b8cbde0
Rururuisi <62675076+Rururuisi@users.noreply.github.com>
2023-09-10 22:14:53 -0400

    .

--- src/store/cart/cart.selector.js
+++ src/store/cart/cart.selector.js
@@ -2,10 +2,10 @@ import { createSelector } from "reselect";
 
 const selectCartReducer = (state) => state.cart;
 
-export const selectCartItems = createSelector([selectCartReducer], (cart) => {
-    console.log(cart.cartItems);
-    return cart.cartItems;
-});
+export const selectCartItems = createSelector(
+    [selectCartReducer],
+    (cart) => cart.cartItems
+);
 
 export const selectIsCartOpen = createSelector(
     [selectCartReducer],
